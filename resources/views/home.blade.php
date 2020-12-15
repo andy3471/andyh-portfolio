0318964575fe0efae0b2ad31d1a4ee072621e77c
Andrew Hargrave <me@andyh.app>
2020-12-15 22:08:18 +0000

    Update to add Neilson Finance role

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,7 +62,8 @@
                     <div class="column is-three-quarters"  data-aos="fade-left">
                     <button class="button is-link is-outlined">Find Out More</button>
                         <p>Hi, I'm Andrew Hargrave. I'm a self taught Web Developer that works with PHP, JS, HTML, SQL (MySQL and MS SQL), Redis, CSS and SCSS. I have a very good knowledge of the Laravel MVC framework, and have used it for multiple personal projects. I use VueJS for as a front-end JavaScript framework primarily, and have released multiple components that can be used freely in any Vue web app. I also have knowledge of Bootstrap and Bulma as UI frameworks.</p>
-                        <p>I am currently employed as an Application Delivery Specialist at Advanced, the UK's third largest software company. I work in the Health & Care business unit with Carenotes, an ASP.NET Web Application, with a large number of SOA services. Carenotes is used widely by the NHS for mental health and community Trusts. I provide third line support for Carenotes, as well as deploy and upgrade it. Carenotes is hosted on Windows Servers with IIS.</p>
+                        <p>I am currently emplayed as an Applications Support Anaylst at Neilson Finance Services. 
+                        <p>Previously, I was employed as an Application Delivery Specialist at Advanced, the UK's third largest software company. I worked in the Health & Care business unit with Carenotes, an ASP.NET Web Application, with a large number of SOA services. Carenotes is used widely by the NHS for mental health and community Trusts. I provide third line support for Carenotes, as well as deploy and upgrade it. Carenotes is hosted on Windows Servers with IIS.</p>
                         <p>I also have a strong knowledge of Linux and Apache, and host multiple web applications on Linux Servers. I host web applications and video game servers privately, which is running on virtualised servers using Proxmox hypervisor, as well as in Docker Containers.</p>
                         <p>Outside of web development and deployment, I like to work on music in my free time, using Analog Synthesizers. I'm also a huge fan of board games and virtual reality.</p>
                         <p>Please check out my live demos on the site, and use the Contact Me form if you would like to work together!</p>
@@ -494,6 +495,19 @@
                         <header class="timeline-header">
                             <span class="tag is-medium is-primary">Today</span>
                         </header>
+                        <div class="timeline-item is-warning" >
+                            <div class="timeline-marker is-warning is-image is-32x32">
+                                <img src="{{ asset('images/neilson.png') }}" alt="neilson">
+                            </div>
+                            <div class="timeline-content" data-aos="zoom-in-left">
+                                <p class="heading">December 2020</p>
+                                <p>Neilson - Application Support Analyst</p>
+                                <ul>
+                                    <li>Create bespoke reports using SQL for stakeholders and managing SSRS reports.</li>
+                                    <li>Troubleshoot both the applications client UI and backend database related issues</li>
+                                </ul>
+                            </div>
+                        </div>
                         <div class="timeline-item is-warning" >
                             <div class="timeline-marker is-warning is-image is-32x32">
                                 <img src="{{ asset('images/advanced.jpg') }}" alt="advanced">
